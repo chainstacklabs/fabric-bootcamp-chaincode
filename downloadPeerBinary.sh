bc8fcf33d9a1a6d4ceb9ee75ff9e7e54e46ce735
gcoupelant <guillaume.coupelant@gmail.com>
2020-07-17 23:38:12 +0800

    Use local "hlf" directory for Fabric binaries and config

--- downloadPeerBinary.sh
+++ downloadPeerBinary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export FABRIC_VERSION=2.1.0
-export FABRIC_PATH=/etc/hyperledger
+export FABRIC_PATH="$(dirname "$0")/hlf"
 export FABRIC_BIN_PATH=${FABRIC_PATH}/bin
 export FABRIC_CFG_PATH=${FABRIC_PATH}/config
 export FABRIC_SOURCES_PATH=${FABRIC_PATH}/sources
@@ -17,6 +17,6 @@ curl -sSL https://github.com/hyperledger/fabric/releases/download/v${FABRIC_VERS
     | tar xz -C ${FABRIC_PATH}
 curl -sSL https://github.com/hyperledger/fabric/archive/v${FABRIC_VERSION}.tar.gz \
     | tar xz -C ${FABRIC_SOURCES_PATH}
-mv ${FABRIC_SOURCES_PATH}/fabric-${FABRIC_VERSION}/sampleconfig/* ${FABRIC_CFG_PATH}
+mv --force ${FABRIC_SOURCES_PATH}/fabric-${FABRIC_VERSION}/sampleconfig/* ${FABRIC_CFG_PATH}
 rm -rf ${FABRIC_SOURCES_PATH}
 ls ${FABRIC_BIN_PATH}
--- webapp/server/cli/scripts/chaincode.sh
+++ webapp/server/cli/scripts/chaincode.sh
@@ -3,7 +3,10 @@ set -e
 
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 
-export FABRIC_CFG_PATH=/etc/hyperledger/config
+FABRIC_PATH="$(dirname "$0")/../../../../hlf"
+
+export FABRIC_BIN_PATH="${FABRIC_PATH}/bin"
+export FABRIC_CFG_PATH="${FABRIC_PATH}/config"
 export ORDERER_CA=$ROOT_PATH/webapp/certs/$ORDERER_CA
 export ORDERER_ADDRESS=$ORDERER_ADDRESS
 export CORE_PEER_TLS_ENABLED=$CORE_PEER_TLS_ENABLED
@@ -17,7 +20,7 @@ export CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID
 export CORE_PEER_TLS_ROOTCERT_FILE=$ROOT_PATH/webapp/certs/$CORE_PEER_TLS_ROOTCERT_FILE
 
 discoverPeers() {
-  /etc/hyperledger/bin/discover \
+   ${FABRIC_BIN_PATH}/discover \
   --peerTLSCA "$CORE_PEER_TLS_ROOTCERT_FILE" \
   --userKey "$ROOT_PATH/webapp/certs/$ADMIN_PRIVATE_KEY" \
   --userCert "$ROOT_PATH/webapp/certs/$ADMIN_CERT" \
@@ -27,7 +30,7 @@ discoverPeers() {
 }
 
 discoverConfig() {
-  /etc/hyperledger/bin/discover \
+  ${FABRIC_BIN_PATH}/discover \
   --peerTLSCA "$CORE_PEER_TLS_ROOTCERT_FILE" \
   --userKey "$ROOT_PATH/webapp/certs/$ADMIN_PRIVATE_KEY" \
   --userCert "$ROOT_PATH/webapp/certs/$ADMIN_CERT" \
@@ -37,15 +40,15 @@ discoverConfig() {
 }
 
 installChaincode() {
-  /etc/hyperledger/bin/peer lifecycle chaincode package "${ROOT_PATH}/${CHAINCODE_NAME}.tar.gz" \
+  ${FABRIC_BIN_PATH}/peer lifecycle chaincode package "${ROOT_PATH}/${CHAINCODE_NAME}.tar.gz" \
   --lang node \
   --path "${ROOT_PATH}/contract" \
   --label "${CHAINCODE_NAME}${CHAINCODE_VERSION}"
-  /etc/hyperledger/bin/peer lifecycle chaincode install "${ROOT_PATH}/${CHAINCODE_NAME}.tar.gz"
+  ${FABRIC_BIN_PATH}/peer lifecycle chaincode install "${ROOT_PATH}/${CHAINCODE_NAME}.tar.gz"
 }
 
 getChaincodePackageID() {
-  PACKAGES=$(/etc/hyperledger/bin/peer lifecycle chaincode queryinstalled | grep "${CHAINCODE_NAME}${CHAINCODE_VERSION}":)
+  PACKAGES=$(${FABRIC_BIN_PATH}/peer lifecycle chaincode queryinstalled | grep "${CHAINCODE_NAME}${CHAINCODE_VERSION}":)
   PACKAGE_ID=${PACKAGES#*Package ID: }
   export PACKAGE_ID=${PACKAGE_ID%,*}
 
@@ -53,7 +56,7 @@ getChaincodePackageID() {
 }
 
 approveChaincode() {
-  /etc/hyperledger/bin/peer lifecycle chaincode approveformyorg \
+  ${FABRIC_BIN_PATH}/peer lifecycle chaincode approveformyorg \
   --name "$CHAINCODE_NAME" \
   --package-id "$PACKAGE_ID" -o "$ORDERER_ADDRESS" \
   --tls \
@@ -66,7 +69,7 @@ approveChaincode() {
 }
 
 checkReadiness() {
-  /etc/hyperledger/bin/peer lifecycle chaincode checkcommitreadiness -o "$ORDERER_ADDRESS" \
+  ${FABRIC_BIN_PATH}/peer lifecycle chaincode checkcommitreadiness -o "$ORDERER_ADDRESS" \
   --channelID "$CHANNEL_ID" \
   --tls \
   --cafile "$ORDERER_CA" \
@@ -80,7 +83,7 @@ checkReadiness() {
 commitChaincode() {
   PEER_ADDRESSES_LIST=(${PEER_ADDRESSES}) && 
   TLS_ROOTCERT_FILES_LIST=(${TLS_ROOTCERT_FILES}) && 
-  /etc/hyperledger/bin/peer lifecycle chaincode commit -o "$ORDERER_ADDRESS" \
+  ${FABRIC_BIN_PATH}/peer lifecycle chaincode commit -o "$ORDERER_ADDRESS" \
   --channelID "$CHANNEL_ID" \
   --name "$CHAINCODE_NAME" \
   --version "$CHAINCODE_VERSION" \
@@ -93,12 +96,12 @@ commitChaincode() {
 }
 
 queryInstalled() {
-  /etc/hyperledger/bin/peer lifecycle chaincode queryinstalled \
+  ${FABRIC_BIN_PATH}/peer lifecycle chaincode queryinstalled \
   --output "${OUTPUT}"
 }
 
 queryCommitted() {
-  /etc/hyperledger/bin/peer lifecycle chaincode querycommitted -o "$ORDERER_ADDRESS" \
+  ${FABRIC_BIN_PATH}/peer lifecycle chaincode querycommitted -o "$ORDERER_ADDRESS" \
   --channelID "$CHANNEL_ID" \
   --tls \
   --cafile "$ORDERER_CA" \
